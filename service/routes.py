fd822aca8b896882dce25910e6c2caefd65ea47d
Karina Defitrah <karinadefitrah20@gmail.com>
2026-01-22 07:48:45 -0500

    implemented list accounts

--- service/routes.py
+++ service/routes.py
@@ -4,74 +4,75 @@ Account Service
 This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
-from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
+from flask import jsonify, request, make_response, abort
 from service.models import Account
-from service.common import status  # HTTP Status Codes
-from . import app  # Import Flask application
+from service.common import status
+from . import app
 
 
-############################################################
+######################################################
 # Health Endpoint
-############################################################
+######################################################
 @app.route("/health")
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
 
 
-######################################################################
+######################################################
 # GET INDEX
-######################################################################
+######################################################
 @app.route("/")
 def index():
     """Root URL response"""
-    return (
-        jsonify(
-            name="Account REST API Service",
-            version="1.0",
-            # paths=url_for("list_accounts", _external=True),
-        ),
-        status.HTTP_200_OK,
-    )
+    return jsonify(
+        name="Account REST API Service",
+        version="1.0",
+    ), status.HTTP_200_OK
 
 
-######################################################################
+######################################################
 # CREATE A NEW ACCOUNT
-######################################################################
+######################################################
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
-    """
-    Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
-    """
+    """Creates an Account"""
     app.logger.info("Request to create an Account")
     check_content_type("application/json")
+
     account = Account()
     account.deserialize(request.get_json())
     account.create()
     message = account.serialize()
-    # Uncomment once get_accounts has been implemented
-    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    location_url = "/"  # Remove once get_accounts has been implemented
+
     return make_response(
-        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+        jsonify(message),
+        status.HTTP_201_CREATED,
+        {"Location": f"/accounts/{account.id}"},
     )
 
-######################################################################
+
+######################################################
 # LIST ALL ACCOUNTS
-######################################################################
+######################################################
+######################################################
+# LIST ALL ACCOUNTS
+######################################################
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """Returns all Accounts"""
+    app.logger.info("Processing all records")
+    accounts = Account.all()
+    results = [account.serialize() for account in accounts]
+    return jsonify(results), status.HTTP_200_OK
 
-# ... place you code here to LIST accounts ...
 
-
-######################################################################
+######################################################
 # READ AN ACCOUNT
-######################################################################
+######################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_account(account_id):
-    """
-    Reads an Account
-    """
+    """Reads an Account"""
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -80,11 +81,32 @@ def get_account(account_id):
             f"Account with id [{account_id}] could not be found."
         )
     return account.serialize(), status.HTTP_200_OK
+
+
+######################################################
+# UTILITY FUNCTIONS
+######################################################
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type == media_type:
+        return
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {media_type}",
+    )
+
+####################################################
+######################################################################
+# LIST ALL ACCOUNTS
+######################################################################
+# READ AN ACCOUNT
+######################################################################
 # ... place you code here to READ an account ...
 
 
 ######################################################################
-# UPDATE AN EXISTING ACCOUNT
+# UPDASTING ACCOUNT
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
@@ -97,18 +119,5 @@ def get_account(account_id):
 # ... place you code here to DELETE an account ...
 
 
-######################################################################
-#  U T I L I T Y   F U N C T I O N S
-######################################################################
+########################################################
 
-
-def check_content_type(media_type):
-    """Checks that the media type is correct"""
-    content_type = request.headers.get("Content-Type")
-    if content_type and content_type == media_type:
-        return
-    app.logger.error("Invalid Content-Type: %s", content_type)
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {media_type}",
-    )
